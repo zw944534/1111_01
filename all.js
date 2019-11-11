9303ace456e140b156457e051955af25e499d71d
Hank Chu <drunkard960027@gmail.com>
2019-11-11 19:29:33 +0800

    add js

---
+++ all.js
@@ -0,0 +1,59 @@
+// let a = 1;
+// setInterval(function(){console.log(a++);},10)
+
+// const http = require("http");
+// http.createServer(function(request,response){
+// 	console.log(request.url);
+// 	console.log("接收到網頁請求");
+// 	response.writeHead(200,{"Content-Type":"text/plain"});
+// 	response.write("Hello World");
+// 	response.end();
+
+// }).listen(3000);
+// console.log("Server已開啟port");
+
+
+
+var http = require("http");
+
+
+//  node.js 載入模組
+
+
+
+http.createServer(function(request, response) {
+  console.log(request.url);
+  if(request.url == '/'){
+// 路由
+    // 資料庫
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>index</h1>");
+    response.end();
+  }else if(request.url == '/search'){
+     console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>search</h1>");
+    response.end();
+  }else if(request.url=='/market'){
+  	console.log("接收到網頁請求！");
+	response.writeHead(200, {"Content-Type": "text/HTML"});  
+  	response.write("<h1>market</h1>");
+  }else if(request.url=='/center'){
+  	console.log("接收到網頁請求！");
+	response.writeHead(200, {"Content-Type": "text/HTML"});  
+  	response.write("<h1>center</h1>");
+  }else if(request.url=='/job'){
+  	console.log("接收到網頁請求！");
+	response.writeHead(200, {"Content-Type": "text/HTML"});  
+  	response.write("<h1>job</h1>");
+  }else{
+  	console.log("接收到網頁請求！");
+	response.writeHead(404, {"Content-Type": "text/HTML"});  
+  	response.write("<h1>error</h1>");
+  }
+
+  
+}).listen(process.env.PORT ||3000);
+console.log("Server已開啟port: 3000.");
+
